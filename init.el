57768cdc111587987dbe06fece72e4a938e5cc94
volumes <vol.umes@mail.ru>
2020-09-14 14:31:13 +0300

    emacs dot files

---
+++ init.el
@@ -0,0 +1,6 @@
+(load "c:/emacs/emacs-26.3-x86_64/.emacs.d/keybindings_my.el")
+(load "c:/emacs/emacs-26.3-x86_64/.emacs.d/window_my.el")
+(load "c:/emacs/emacs-26.3-x86_64/.emacs.d/typescript.el")
+
+(load "c:/emacs/emacs-26.3-x86_64/.emacs.d/package_my.el")
+
